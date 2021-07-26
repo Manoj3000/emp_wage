a4d32490cf960cf707047b711bc8c39e5f814fa8
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-27 03:12:52 +0530

    Added: condition of max working hours and max working days

--- emp_wage.sh
+++ emp_wage.sh
@@ -9,27 +9,42 @@ full_day_hrs=8
 part_time_hrs=4
 month=20
 month_wage=0
+working_hrs=0
+max_working_hrs=100
+working_days=0
+max_working_days=20
 
 
-for (( i=1; i<=$month; i++ ))
+while true
 do
-    random_check=$((RANDOM%3))
-    
-    case $random_check in
-    1)
-        total_wage=$(( $per_hrs * $full_day_hrs ))
-        echo "Employee is present for Full time wage is : $total_wage"
-        month_wage=$(( $month_wage + $total_wage ))
-        ;;
-    2)
-        part_wage=$(( $per_hrs * $part_time_hrs ))
-        echo "Employee is present for part time wage is : $part_wage"
-        month_wage=$(( $month_wage + $part_wage ))
-        ;;
-    *)
-        echo "Employee is absent and wage is : 0"
-        ;;
-    esac
+
+    if (( $working_hrs < $max_working_hrs && $working_days < $max_working_days ))
+    then
+        random_check=$((RANDOM%3))
+        
+        case $random_check in
+        1)
+            total_wage=$(( $per_hrs * $full_day_hrs ))
+            echo "Employee is present for Full time wage is : $total_wage"
+            month_wage=$(( $month_wage + $total_wage ))
+            working_hrs=$(( $working_hrs + $full_day_hrs ))
+            working_days=$(( $working_days + 1 ))
+            ;;
+        2)
+            part_wage=$(( $per_hrs * $part_time_hrs ))
+            echo "Employee is present for part time wage is : $part_wage"
+            month_wage=$(( $month_wage + $part_wage ))
+            working_hrs=$(( $working_hrs + $part_time_hrs ))
+            working_days=$(( $working_days + 1 ))
+            ;;
+        *)
+            echo "Employee is absent and wage is : 0"
+            ;;
+        esac
+    else
+        echo "Working days are : $working_days and Working hours are : $working_hrs"
+        break
+    fi
 done
 
 echo "Employee wage of month is : $month_wage"
