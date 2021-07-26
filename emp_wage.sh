c9e8f6f911772f857899d7394eba9ee97183594a
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-27 02:34:49 +0530

    Added: part time employee wage

--- emp_wage.sh
+++ emp_wage.sh
@@ -3,15 +3,21 @@
 echo "Welcome to in emp_wage program"
 
 is_present=1
+is_part_time=2
 per_hrs=20
-day_hrs=8
+full_day_hrs=8
+part_time_hrs=4
 
-random_check=$(((RANDOM%2)+1))
+random_check=$((RANDOM%3))
 
 if [ $is_present -eq $random_check ]
 then
-    total_wage=$(( $per_hrs * $day_hrs ))
-    echo "Employee is present and todays wage is : $total_wage"
+    total_wage=$(( $per_hrs * $full_day_hrs ))
+    echo "Full time wage is : $total_wage"
+elif [ $is_part_time -eq $random_check ]
+then
+    part_wage=$(( $per_hrs * $part_time_hrs ))
+    echo "Part time wage is : $part_wage"
 else
     echo "Employee is absent and todays wage is : 0"
 fi
